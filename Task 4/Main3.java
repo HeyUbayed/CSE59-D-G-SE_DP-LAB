98e59f79a17f41fbaf5221865930091467ef231f
HeyUbayed <ubaid.8r@gmail.com>
2026-07-02 16:26:40 +0600

    Update Main3.java

--- Task 4/Main3.java
+++ Task 4/Main3.java
@@ -1,1 +1,46 @@
+interface MessageSender{
+    void sendMessage(String message);
+}
+class EmailSender implements MessageSender{
+    @Override 
+    public void sendMessage(String message)
+    {
+        System.out.println("Sending email: " + message);
+    }
+}
 
+class NotificationService{
+    private MessageSender messgeSender;
+
+    public NotificationService(MessageSender messageSender)
+    {
+        this.messgeSender = messageSender;
+    }
+
+    public void alertUser(String msg)
+    {
+        messgeSender.sendMessage(msg);
+    }
+}
+
+class SMSSender implements MessageSender{
+    @Override
+    public void sendMessage(String message)
+    {
+        System.out.println("Sending SMS: " + message);
+    }
+}
+
+public class Main3 {
+    public static void main(String[] args)
+    {
+        MessageSender email = new EmailSender();
+        NotificationService emailNotification = new NotificationService(email);
+
+        emailNotification.alertUser("Your order has been shipped.");
+
+        MessageSender sms = new SMSSender();
+        NotificationService smsNotification = new NotificationService(sms);
+        smsNotification.alertUser("Your OTP is 123456.");
+    }
+}
