de5bff4da8311943298f7b4d1abfd921608389ed
HeyUbayed <ubaid.8r@gmail.com>
2026-07-02 16:26:03 +0600

    Update Main1.java

--- Task 4/Main1.java
+++ Task 4/Main1.java
@@ -1,1 +1,49 @@
+class TeamMember{
+    protected String name;
 
+    public TeamMember(String name)
+    {
+        this.name = name;
+    }
+}
+
+class Employee extends TeamMember {
+    public Employee(String name)
+    {
+        super(name);
+    }
+
+    public void calculateBonus()
+    {
+        System.out.println("Calculating employee bonus.");
+    }
+}
+
+class Contractor extends TeamMember{
+    public Contractor(String name)
+    {
+        super(name);
+    }
+    public void submitInvoice(){
+        System.out.println("Submitting contractor invoice.");
+    }
+}
+public class Main{
+    public static void main(String[] args)
+    {
+        TeamMember member = new TeamMember("Ubaid");
+        System.out.println("Member name: " + member.name);
+        System.out.println();
+
+        Employee emp = new Employee("Kadir");
+        System.out.println("Employee name: " + emp.name);
+        emp.calculateBonus();
+        System.out.println();
+
+        Contractor cnt = new Contractor("Joshim");
+        System.out.println("Contractor name: " + cnt.name);
+        cnt.submitInvoice();
+        System.out.println();
+
+    }
+}
